f005b564ff7ede0ddaf9fffadce4a61c684afa8e
Nathachai Thongniran <inid3a@gmail.com>
2017-09-09 07:49:16 +0700

    Add automated script to build package

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const clean = require('gulp-clean');
+const fs = require('fs');
+
+const mainFile = 'promptpay.php';
+const mainFileContent = fs.readFileSync(mainFile, 'utf8');
+const pluginVersion = /^Version.*$/igm.exec(mainFileContent)[0].substring(9).trim();
+const pluginName = /^Plugin Name.*$/igm.exec(mainFileContent)[0].substring(13).trim().toLowerCase();
+const packageFolderName = pluginName + '-' + pluginVersion;
+
+gulp.task('clean', function () {
+  return gulp.src(pluginName + '-*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('pack', ['clean'], function () {
+  return gulp.src([
+    'css/**',
+    'js/main.min.js',
+    'promptpay.php',
+    'readme.txt',
+    'screenshot-1.jpg'
+  ], {base: '.'})
+    .pipe(gulp.dest(packageFolderName));
+});
+
+gulp.task('pack.zip', ['pack'], function () {
+  return gulp.src(packageFolderName + '/**')
+    .pipe(zip(packageFolderName + '.zip'))
+    .pipe(gulp.dest('./'));
+});
